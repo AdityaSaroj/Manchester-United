f5be94c69a7d898321a2f6ab6fdd4b7a882ec1d0
Aditya Saroj <adityasaroj001@gmail.com>
2020-05-08 00:55:31 +0530

    Initial commit

---
+++ JS/news.js
@@ -0,0 +1,67 @@
+//Since I am using a free API, can't use this more than 100 times a day
+
+fetch(
+  "https://gnews.io/api/v3/search?q=Manchester%20United&token="YOUR-API-KEY""
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    HTML(data);
+  });
+
+const HTML = (data) => {
+  console.log(data);
+  document.getElementById("news").innerHTML = `      <div class="cards">
+  <div class="card">
+    <h1>${data.articles[0].title}</h1>
+    <p>
+      <img
+        src="${data.articles[0].image}"
+        alt="image"
+      />${data.articles[0].description}
+    </p>
+    <a href="${data.articles[0].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[1].title}</h1>
+    <p>
+      <img src="${data.articles[1].image}" alt="image" />
+      ${data.articles[1].description}
+    </p>
+    <a href="${data.articles[1].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[2].title}</h1>
+    <p><img src="${data.articles[2].image}" alt="image" />${data.articles[2].description}</p>    <a href="${data.articles[2].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[3].title}</h1>
+    <p><img src="${data.articles[3].image}" alt="image" />${data.articles[3].description}</p>    <a href="${data.articles[3].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[4].title}</h1>
+    <p><img src="${data.articles[4].image}" alt="image" />${data.articles[4].description}</p>    <a href="${data.articles[4].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[5].title}</h1>
+    <p><img src="${data.articles[5].image}" alt="image" />${data.articles[5].description}</p>    <a href="${data.articles[5].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[6].title}</h1>
+    <p><img src="${data.articles[6].image}" alt="image" />${data.articles[6].description}</p>    <a href="${data.articles[6].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[7].title}</h1>
+    <p><img src="${data.articles[7].image}" alt="image" />${data.articles[7].description}</p>    <a href="${data.articles[7].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[8].title}</h1>
+    <p><img src="${data.articles[8].image}" alt="image" />${data.articles[8].description}</p>    <a href="${data.articles[8].url}" target="_blank">Read More</a>
+  </div>
+  <div class="card">
+    <h1>${data.articles[9].title}</h1>
+    <p><img src="${data.articles[9].image}" alt="image" />${data.articles[9].description}</p>    <a href="${data.articles[9].url}" target="_blank">Read More</a>
+  </div>
+</div>`;
+};
---
+++ JS/script.js
@@ -0,0 +1,86 @@
+//Form Validation
+function validateConForm() {
+  x = document.forms["contact"]["name"].value;
+  var regex = /^[a-zA-Z]+$/;
+  if (x == "") {
+    alert("Name cannot be empty!");
+    return false;
+  } else if (regex.test(x) == false) {
+    alert("Name must contain letters only!");
+    return false;
+  }
+
+  x = document.forms["contact"]["mail"].value;
+  var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+  if (regex.test(x) == false) {
+    alert("Please enter a valid e-mail address!");
+    return false;
+  }
+
+  x = document.forms["contact"]["feedback"].value;
+  if (x == "") {
+    alert("We'd love some words from you!");
+    return false;
+  }
+}
+
+//For Manchester United is... footer
+
+var TxtRotate = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("txt-rotate");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-rotate");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
